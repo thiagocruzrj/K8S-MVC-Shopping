8101141c6a045140d5b0e3639f7301387685aff3
Thiago Cruz <thagocruz@gmail.com>
2021-10-18 18:15:08 -0300

    update some files

--- Shopping.Client/Startup.cs
+++ Shopping.Client/Startup.cs
@@ -49,7 +49,9 @@ namespace Shopping.Client
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
