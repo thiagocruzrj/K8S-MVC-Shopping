594f57ee6f46bdd16c86d448d983c9798189e5de
Thiago Cruz <thagocruz@gmail.com>
2021-10-18 18:02:00 -0300

    Create HomeController.cs

---
+++ Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+    }
+}
