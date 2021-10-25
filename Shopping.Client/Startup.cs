a308b3cbdf420aa73f59c9a8ad7c037ebb226137
Thiago Cruz <thagocruz@gmail.com>
2021-10-25 15:15:30 -0300

    Update Startup.cs

--- Shopping.Client/Startup.cs
+++ Shopping.Client/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Shopping.Client
 {
@@ -15,13 +16,15 @@ namespace Shopping.Client
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("ShoppingAPIClient", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:5000");
+            });
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,7 +34,6 @@ namespace Shopping.Client
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
